e9cc2f4e98f93435703e5eb7297e18146aa2c2e8
Bruno Fagundez <bfagundez@gmail.com>
2014-04-17 16:29:02 -0300

    cleanup

--- webapp.py
+++ webapp.py
@@ -3,24 +3,18 @@ from canvas_signed_request import SignedRequest
 app = Flask(__name__)
 import os 
 
-
 @app.route("/")
 def hello():
     return "Hello World!"
 
 # add a new route
-@app.route('/canvas', methods=['POST','GET'])
+@app.route('/canvas', methods=['POST'])
 def canvas():
-  print 'received signed request'
   secret = os.environ.get('SECRET')
-  print ' secret '+secret
   sr_param = request.form['signed_request']
-  print ' sr param '+sr_param
   srHelper = SignedRequest(secret,sr_param)
   canvasRequestJSON = srHelper.verifyAndDecode()
-
-  #load request data json to extract parameters
-  return str(canvasRequestJSON)
+  return canvasRequestJSON
 
 if __name__ == "__main__":
     app.run()
